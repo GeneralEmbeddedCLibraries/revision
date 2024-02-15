0991e36b4b8a23f968a6d8be5e51e470c227e52c
Žiga Miklošič <ziga.miklosic@seltron.si>
2024-02-15 08:15:53 +0100

    - replace tabs with spaces

--- src/version.c
+++ src/version.c
@@ -37,39 +37,39 @@
 #define VER_APP_HEADER_VER          ( 2 )
 
 /**
- * 	Convert value to string
+ *     Convert value to string
  */
-#define VER_STR_HELPER(x) 		#x
-#define VER_STR(x) 				VER_STR_HELPER(x)
+#define VER_STR_HELPER(x)           #x
+#define VER_STR(x)                  VER_STR_HELPER(x)
 
 ////////////////////////////////////////////////////////////////////////////////
 // Variables
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * 	Software string and number
+ *     Software string and number
  */
-static const char *		gs_sw_ver_str 			= "Software Version " VER_STR(VER_SW_MAJOR) "." VER_STR(VER_SW_MINOR) "." VER_STR(VER_SW_DEVELOP) "." VER_STR(VER_SW_TEST) " (Builted on " __DATE__ " " __TIME__ ")";
-static const uint32_t 	gu32_sw_ver_num			= (( VER_SW_MAJOR << 24 ) | ( VER_SW_MINOR << 16 ) | ( VER_SW_DEVELOP << 8 ) | ( VER_SW_TEST ));
+static const char *     gs_sw_ver_str   = "Software Version " VER_STR(VER_SW_MAJOR) "." VER_STR(VER_SW_MINOR) "." VER_STR(VER_SW_DEVELOP) "." VER_STR(VER_SW_TEST) " (Builted on " __DATE__ " " __TIME__ ")";
+static const uint32_t   gu32_sw_ver_num = (( VER_SW_MAJOR << 24 ) | ( VER_SW_MINOR << 16 ) | ( VER_SW_DEVELOP << 8 ) | ( VER_SW_TEST ));
 
 /**
  *  Hardware string and number
  */
-static const char *     gs_hw_ver_str           = "Hardware Version " VER_STR(VER_HW_MAJOR) "." VER_STR(VER_HW_MINOR) "." VER_STR(VER_HW_DEVELOP) "." VER_STR(VER_HW_TEST);
-static const uint32_t   gu32_hw_ver_num         = (( VER_HW_MAJOR << 24 ) | ( VER_HW_MINOR << 16 ) | ( VER_HW_DEVELOP << 8 ) | ( VER_HW_TEST ));
+static const char *     gs_hw_ver_str   = "Hardware Version " VER_STR(VER_HW_MAJOR) "." VER_STR(VER_HW_MINOR) "." VER_STR(VER_HW_DEVELOP) "." VER_STR(VER_HW_TEST);
+static const uint32_t   gu32_hw_ver_num = (( VER_HW_MAJOR << 24 ) | ( VER_HW_MINOR << 16 ) | ( VER_HW_DEVELOP << 8 ) | ( VER_HW_TEST ));
 
 
 /**
- * 	Application header informations
+ *     Application header informations
  */
 static volatile const ver_app_header_t __attribute__ (( section( VER_APP_HEAD_SECTION ))) g_app_header =
 {
     .sw_ver     = gu32_sw_ver_num,
-	.hw_ver     = gu32_hw_ver_num,
-	.app_size	= 0,	                /* Calculated by post-build script */
-	.app_crc	= 0,	                /* Calculated by post-build script */
-	.ver        = VER_APP_HEADER_VER,
-	.crc        = 0,                    /* Calculated by post-build script */
+    .hw_ver     = gu32_hw_ver_num,
+    .app_size   = 0,                    /* Calculated by post-build script */
+    .app_crc    = 0,                    /* Calculated by post-build script */
+    .ver        = VER_APP_HEADER_VER,
+    .crc        = 0,                    /* Calculated by post-build script */
 };
 
 
@@ -79,116 +79,116 @@ static volatile const ver_app_header_t __attribute__ (( section( VER_APP_HEAD_SE
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * 	@brief		Get software version
+ *  @brief  Get software version
  *
- * @param[out]	p_major		- Pointer to major SW version
- * @param[out]	p_minor		- Pointer to minor SW version
- * @param[out]	p_develop	- Pointer to develop SW version
- * @param[out]	p_test		- Pointer to test SW version
- * @return 		sw_ver		- Complete 32-bit number of SW version
+ * @param[out]  p_major     - Pointer to major SW version
+ * @param[out]  p_minor     - Pointer to minor SW version
+ * @param[out]  p_develop   - Pointer to develop SW version
+ * @param[out]  p_test      - Pointer to test SW version
+ * @return      sw_ver      - Complete 32-bit number of SW version
  */
 ////////////////////////////////////////////////////////////////////////////////
 uint32_t version_get_sw(uint8_t * const p_major, uint8_t * const p_minor, uint8_t * const p_develop, uint8_t * const p_test)
 {
-	const uint32_t sw_ver = gu32_sw_ver_num;
+    const uint32_t sw_ver = gu32_sw_ver_num;
 
-	if ( NULL != p_major )
-	{
-		*p_major = (uint8_t)(( gu32_sw_ver_num >> 24U ) & 0xFFU );
-	}
-	if ( NULL != p_minor )
-	{
-		*p_minor = (uint8_t)(( gu32_sw_ver_num >> 16U ) & 0xFFU );
-	}
-	if ( NULL != p_develop )
-	{
-		*p_develop = (uint8_t)(( gu32_sw_ver_num >> 8U ) & 0xFFU );
-	}
-	if ( NULL != p_test )
-	{
-		*p_test = (uint8_t)(( gu32_sw_ver_num >> 0U ) & 0xFFU );
-	}
+    if ( NULL != p_major )
+    {
+        *p_major = (uint8_t)(( gu32_sw_ver_num >> 24U ) & 0xFFU );
+    }
+    if ( NULL != p_minor )
+    {
+        *p_minor = (uint8_t)(( gu32_sw_ver_num >> 16U ) & 0xFFU );
+    }
+    if ( NULL != p_develop )
+    {
+        *p_develop = (uint8_t)(( gu32_sw_ver_num >> 8U ) & 0xFFU );
+    }
+    if ( NULL != p_test )
+    {
+        *p_test = (uint8_t)(( gu32_sw_ver_num >> 0U ) & 0xFFU );
+    }
 
-	return sw_ver;
+    return sw_ver;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * 	@brief		Get hardware version
+ *  @brief  Get hardware version
  *
- * @param[out]	p_major		- Pointer to major HW version
- * @param[out]	p_minor		- Pointer to minor HW version
- * @param[out]	p_develop	- Pointer to develop HW version
- * @param[out]	p_test		- Pointer to test HW version
- * @return 		sw_ver		- Complete 32-bit number of HW version
+ * @param[out]  p_major     - Pointer to major HW version
+ * @param[out]  p_minor     - Pointer to minor HW version
+ * @param[out]  p_develop   - Pointer to develop HW version
+ * @param[out]  p_test      - Pointer to test HW version
+ * @return      sw_ver      - Complete 32-bit number of HW version
  */
 ////////////////////////////////////////////////////////////////////////////////
 uint32_t version_get_hw(uint8_t * const p_major, uint8_t * const p_minor, uint8_t * const p_develop, uint8_t * const p_test)
 {
-	const uint32_t hw_ver = gu32_hw_ver_num;
+    const uint32_t hw_ver = gu32_hw_ver_num;
 
-	if ( NULL != p_major )
-	{
-		*p_major = (uint8_t)(( gu32_hw_ver_num >> 24U ) & 0xFFU );
-	}
-	if ( NULL != p_minor )
-	{
-		*p_minor = (uint8_t)(( gu32_hw_ver_num >> 16U ) & 0xFFU );
-	}
-	if ( NULL != p_develop )
-	{
-		*p_develop = (uint8_t)(( gu32_hw_ver_num >> 8U ) & 0xFFU );
-	}
-	if ( NULL != p_test )
-	{
-		*p_test = (uint8_t)(( gu32_hw_ver_num >> 0U ) & 0xFFU );
-	}
+    if ( NULL != p_major )
+    {
+        *p_major = (uint8_t)(( gu32_hw_ver_num >> 24U ) & 0xFFU );
+    }
+    if ( NULL != p_minor )
+    {
+        *p_minor = (uint8_t)(( gu32_hw_ver_num >> 16U ) & 0xFFU );
+    }
+    if ( NULL != p_develop )
+    {
+        *p_develop = (uint8_t)(( gu32_hw_ver_num >> 8U ) & 0xFFU );
+    }
+    if ( NULL != p_test )
+    {
+        *p_test = (uint8_t)(( gu32_hw_ver_num >> 0U ) & 0xFFU );
+    }
 
-	return hw_ver;
+    return hw_ver;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * 	@brief		Get SW version string
+ *  @brief  Get SW version string
  *
- * @return 		gs_sw_ver_str - Software version string
+ * @return  gs_sw_ver_str - Software version string
  */
 ////////////////////////////////////////////////////////////////////////////////
-const char*	version_get_sw_str(void)
+const char* version_get_sw_str(void)
 {
-	return gs_sw_ver_str;
+    return gs_sw_ver_str;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * 	@brief		Get HW version string
+ *  @brief  Get HW version string
  *
- * @return 		gs_hw_ver_str - Software version string
+ * @return  gs_hw_ver_str - Software version string
  */
 ////////////////////////////////////////////////////////////////////////////////
-const char*	version_get_hw_str(void)
+const char* version_get_hw_str(void)
 {
-	return gs_hw_ver_str;
+    return gs_hw_ver_str;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * 	@brief		Get project detailed information string
+ *  @brief  Get project detailed information string
  *
- * @return 		proj_info_str - Project info string
+ * @return proj_info_str - Project info string
  */
 ////////////////////////////////////////////////////////////////////////////////
 const char* version_get_proj_info_str(void)
 {
-	const char* proj_info_str = NULL;
+    const char* proj_info_str = NULL;
 
-	#if ( 1 == VER_CFG_USE_PROJ_INFO_EN )
-		proj_info_str = proj_info_get_str();
-	#else
-		proj_info_str = "Not used...";
-	#endif
+    #if ( 1 == VER_CFG_USE_PROJ_INFO_EN )
+        proj_info_str = proj_info_get_str();
+    #else
+        proj_info_str = "Not used...";
+    #endif
 
-	return proj_info_str;
+    return proj_info_str;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
